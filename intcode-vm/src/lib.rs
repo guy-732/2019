684e8844be05ca6643322dca647e844741c70e3a
guy-732 <81652178+guy-732@users.noreply.github.com>
2023-12-26 16:10:18 +0100

    IntcodeVM: Minor refactoring
    The refactoring makes it easier to implement opcode 03 (input) and opcode 04 (output)

--- intcode-vm/src/lib.rs
+++ intcode-vm/src/lib.rs
@@ -3,14 +3,15 @@
 //! # Examples
 //!
 //! ```
-//! # use intcode_vm::IntcodeVM;
+//! use intcode_vm::{IntcodeVM, VMResult};
+//!
 //! let mut vm = IntcodeVM::new([1, 0, 0, 3, 99]);
-//! vm.run().unwrap();
+//! assert_eq!(vm.run().unwrap(), VMResult::Halted);
 //! assert!(vm.into_memory().memory_starts_with(&[1, 0, 0, 2, 99]))
 //! ```
 //!
 //! ```
-//! # use intcode_vm::IntcodeVM;
+//! # use intcode_vm::{IntcodeVM, VMResult};
 //! let mut vm = IntcodeVM::new([
 //!     1, 9, 10, 3, // add values at position 9 and 10 together and put that at position 3
 //!     2, 3, 11, 0, // multiplies values at position 3 and 11 together and put that at position 0
@@ -18,7 +19,7 @@
 //!     30, 40, 50   // data, not instructions
 //! ]);
 //!
-//! vm.run().unwrap();
+//! assert_eq!(vm.run().unwrap(), VMResult::Halted);
 //! assert!(vm.into_memory().memory_starts_with(&[
 //!     3500, 9, 10, 70,
 //!     2, 3, 11, 0,
@@ -32,6 +33,7 @@ pub mod memory;
 pub mod vm;
 
 pub use vm::IntcodeVM;
+pub use vm::VMResult;
 
 #[cfg(test)]
 mod tests {
@@ -40,28 +42,28 @@ mod tests {
     #[test]
     fn test_add() {
         let mut vm = IntcodeVM::new([1, 0, 0, 0, 99]);
-        vm.run().unwrap();
+        assert_eq!(vm.run().unwrap(), VMResult::Halted);
         assert!(vm.into_memory().memory_starts_with(&[2, 0, 0, 0, 99]));
     }
 
     #[test]
     fn test_mul1() {
         let mut vm = IntcodeVM::new([2, 3, 0, 3, 99]);
-        vm.run().unwrap();
+        assert_eq!(vm.run().unwrap(), VMResult::Halted);
         assert!(vm.into_memory().memory_starts_with(&[2, 3, 0, 6, 99]));
     }
 
     #[test]
     fn test_mul2() {
         let mut vm = IntcodeVM::new([2, 4, 4, 5, 99, 0]);
-        vm.run().unwrap();
+        assert_eq!(vm.run().unwrap(), VMResult::Halted);
         assert!(vm.into_memory().memory_starts_with(&[2, 4, 4, 5, 99, 9801]));
     }
 
     #[test]
     fn test_add_into_mul_dynamic() {
         let mut vm = IntcodeVM::new([1, 1, 1, 4, 99, 5, 6, 0, 99]);
-        vm.run().unwrap();
+        assert_eq!(vm.run().unwrap(), VMResult::Halted);
         assert!(vm
             .into_memory()
             .memory_starts_with(&[30, 1, 1, 4, 2, 5, 6, 0, 99]));
